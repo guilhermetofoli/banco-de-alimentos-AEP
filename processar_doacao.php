1853b7af4d41fcdf3a8e093be077f692890f9cf1
Guizorrr <guitofoli1806@gmail.com>
2025-10-16 22:32:10 -0300

    Implementa a lógica de processamento e inserção de doações na tabela de transações.

---
+++ processar_doacao.php
@@ -0,0 +1,28 @@
+<?php
+require_once 'conexao.php';
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $doador_id = mysqli_real_escape_string($conexao, $_POST['fk_id_doador']);
+    $alimento_id = mysqli_real_escape_string($conexao, $_POST['fk_id_alimento']);
+    $instituicao_id = mysqli_real_escape_string($conexao, $_POST['fk_id_instituicao']);
+    $quantidade = mysqli_real_escape_string($conexao, $_POST['quantidade']);
+    
+    // Query de INSERT na tabela DOACOES
+    $sql = "INSERT INTO doacoes (fk_id_doador, fk_id_alimento, fk_id_instituicao, quantidade) 
+            VALUES ('$doador_id', '$alimento_id', '$instituicao_id', '$quantidade')";
+
+    if (mysqli_query($conexao, $sql)) {
+        // Sucesso: Redireciona para a lista de Doações (próximo passo)
+        mysqli_close($conexao);
+        header("Location: listar_doacoes.php?sucesso=true");
+        exit();
+    } else {
+        $erro = "Erro ao registrar a doação: " . mysqli_error($conexao);
+        mysqli_close($conexao);
+        die("<h2>Erro de Transação</h2><p>$erro</p><p><a href='registrar_doacao_form.php'>Voltar</a></p>");
+    }
+} else {
+    header("Location: registrar_doacao_form.php");
+    exit();
+}
+?>
